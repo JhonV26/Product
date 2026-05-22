945396ef5612e6ea38ddb3870bea21f24ffd7094
Jhon <jhon26vasquezm@gmail.com>
2026-05-22 11:11:33 -0500

    Agregar todos los archivos del proyecto (clases Product y App)

---
+++ src/App.java
@@ -0,0 +1,26 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        Product product1 = new Product();
+
+        product1.setId(1);
+        product1.setName("Laptop");
+        product1.setPrice(999.99);
+        product1.setStock(10);
+
+        Product product2 = new Product(2, "Smartphone", 499.99, 20);
+
+        product1.setPrice(1200);
+        product2.setStock(15);
+
+        System.out.println(product1.getName());
+        System.out.println(product1.getPrice());
+
+        System.out.println(product2.getName());
+        System.out.println(product2.getStock());
+
+        System.out.println(product1);
+        System.out.println(product2);
+    }
+}
